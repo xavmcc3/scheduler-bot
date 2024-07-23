61b0bbce495899782ff4199189d36ab58b21dbdd
Xavier McClurkin <xavmcc3@gmail.com>
2024-07-23 09:41:23 -0400

    fixed help command

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,11 +1,26 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 
 const data = new SlashCommandBuilder()
     .setName('help')
     .setDescription('Gives details on how to use the application.');
 
+const profileimage = new AttachmentBuilder('./res/schedule-pfp.png', { name: 'pfp.png' });
+
 async function execute(interaction) {
-    await interaction.reply('`throw new NotImplementedException();`');
+    const embed = new EmbedBuilder()
+    .setColor(0x52FF80)
+	.setTitle('Upcoming Session')
+	.setAuthor({ name: 'Scheduler', iconURL: 'attachment://pfp.png' })
+    .setDescription('Use the application by typing `/` followed by the name of a command.')
+    .addFields(
+		{ name: '**Commands**', value: '**`/session`** See the details for the upcoming session.\n**`/confirm`** Let people know you\'re going to the next session.\n**`/unconfirm`** If things changed or you accidentally confirmed, use this to unconfirm.\n**`/help`** Shows this message.' }
+    )
+    .setTimestamp()
+
+    await interaction.reply({
+        embeds: [embed],
+        files: [profileimage]
+    });
 }
 
 module.exports = {
